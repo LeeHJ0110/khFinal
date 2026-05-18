17cacee097847e3e89c5b145bdb771c35f59ba42
DESKTOP-R944F2E\user1 <hello>
2026-05-18 12:47:42 +0900

    feat : BoardEntity 1차 작성

--- backEnd/app/src/main/java/com/kh/app/board/controller/BoardController.java
+++ backEnd/app/src/main/java/com/kh/app/board/controller/BoardController.java
@@ -1,4 +1,17 @@
 package com.kh.app.board.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/board")
+@RequiredArgsConstructor
+@Slf4j
 public class BoardController {
+
+    @PostMapping("/api/new")
+    public void write(){}
 }
