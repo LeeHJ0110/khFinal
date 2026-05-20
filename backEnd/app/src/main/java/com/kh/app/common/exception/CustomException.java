423ac8adf797c8030b80fe09b7631291ef9141b9
DESKTOP-G1FQO3G\user1 <ansanghyun>
2026-05-20 15:36:00 +0900

    fix   :  MemberRepository 미비한 부분 추가 common 에 exception 패키지 하위 파일에  Errocode, CustomException 추가

---
+++ backEnd/app/src/main/java/com/kh/app/common/exception/CustomException.java
@@ -0,0 +1,12 @@
+package com.kh.app.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+}
--- backEnd/app/src/main/java/com/kh/app/member/service/MemberService.java
+++ backEnd/app/src/main/java/com/kh/app/member/service/MemberService.java
@@ -1,7 +1,9 @@
 package com.kh.app.member.service;
 
+import com.kh.app.common.exception.CustomException;
 import com.kh.app.member.dto.request.MemberJoinReqDto;
 import com.kh.app.member.entity.MemberEntity;
+import com.kh.app.member.exception.MemberErrorCode;
 import com.kh.app.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +22,18 @@ public class MemberService {
 
     @Transactional
     public void join(MemberJoinReqDto dto) {
+        if (memberRepository.existsByMemberUsername(dto.getMemberUsername())) {
+            throw new CustomException(MemberErrorCode.DUPLICATE_USERNAME);
+        }
 
+        if (memberRepository.existsByMemberNickname(dto.getMemberNickname())) {
+            throw new CustomException(MemberErrorCode.DUPLICATE_NICKNAME);
+        }
+
+        String encodedPassword = passwordEncoder.encode(dto.getMemberPassword());
+        MemberEntity entity = dto.toEntity(encodedPassword);
+        memberRepository.save(entity);
+        log.info("[회원가입 완료] username : {}", dto.getMemberUsername());
     }
 
 }
