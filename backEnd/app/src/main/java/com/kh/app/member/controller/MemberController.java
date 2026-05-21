2d4561a84afd971d62127c1d37ae9350e2a7d838
DESKTOP-G1FQO3G\user1 <ansanghyun>
2026-05-21 16:36:37 +0900

    docs   :  권한 설정 변경

--- backEnd/app/src/main/java/com/kh/app/member/controller/MemberController.java
+++ backEnd/app/src/main/java/com/kh/app/member/controller/MemberController.java
@@ -9,10 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "회원" , description = "회원 관련 API")
 @RestController
@@ -33,6 +30,16 @@ public class MemberController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .build();
+    }
+
+    @GetMapping("check-username")
+    public boolean checkUsername(String username){
+        return memberService.checkUsername(username);
+    }
+
+    @GetMapping("check-nickname")
+    public boolean checkNickname(String nickname){
+        return memberService.checkNickname(nickname);
     }
 }
 
