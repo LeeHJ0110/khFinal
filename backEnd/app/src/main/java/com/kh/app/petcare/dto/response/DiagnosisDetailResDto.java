fb9059d02c395a969700d1701a952f8526c8e328
stock <kimuser050>
2026-05-25 19:16:30 +0900

    feat: back paging, 수의사 목록보기, 상세보기 추가

--- backEnd/app/src/main/java/com/kh/app/petcare/dto/response/DiagnosisDetailResDto.java
+++ backEnd/app/src/main/java/com/kh/app/petcare/dto/response/DiagnosisDetailResDto.java
@@ -1,23 +1,33 @@
 package com.kh.app.petcare.dto.response;
 
 import com.kh.app.common.entity.DelYn;
+import com.kh.app.petcare.dto.request.DiagnosisAnswerDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
-//수의사 목록 화면용
-public class DiagnosisResDto {
 
+public class DiagnosisDetailResDto {
+
+    // 진단 신청 번호
     private Long diagnosisReqId;
 
+    // 펫 번호
     private Long petId;
 
+    // 진행 상태
     private DelYn diagnosisReqStatus;
 
+    // 신청일
     private LocalDateTime createdAt;
+
+    // 답변 목록
+    private List<DiagnosisAnswerDto> answerList;
 }
+
