80c28516e74d874c4c71d7a4aaf9ea76f291bc12
DESKTOP-R944F2E\user1 <hello>
2026-05-26 11:44:23 +0900

    feat : BoardErrorCode 작성

---
+++ backEnd/app/src/main/java/com/kh/app/board/exception/BoardErrorCode.java
@@ -0,0 +1,17 @@
+package com.kh.app.board.exception;
+
+import com.kh.app.common.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum BoardErrorCode implements ErrorCode {
+
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+    BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시글입니다.");
+
+    private final HttpStatus status;
+    private final String msg;
+}
