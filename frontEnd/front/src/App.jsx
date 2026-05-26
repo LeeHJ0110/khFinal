c6e4193371484f227ddee41dcf282c37a80233f8
DESKTOP-G1FQO3G\user1 <ansanghyun>
2026-05-26 14:31:55 +0900

    - docs   :  app.jsx 로그인 추가

--- frontEnd/front/src/App.jsx
+++ frontEnd/front/src/App.jsx
@@ -9,16 +9,24 @@ import DiagnosisRequestHomePage from "./pages/petcare/DiagnosisRequestHomePage";
 import DiagnosisRequestPage from "./pages/petcare/DiagnosisRequestPage";
 import DiagnosisManagePage from "./pages/petcare/DiagnosisManagePage";
 import DiagnosisDetailPage from "./pages/petcare/DiagnosisDetailPage";
+import MemberJoinTermsPage from "./pages/member/MemberJoinTermsPage";
+import MemberJoinCompletePage from "./pages/member/MemberJoinCompletePage";
+import MemberKakaoJoinPage from "./pages/member/MemberKakaoJoinPage";
+import MemberKakaoCallbackPage from "./pages/member/MemberKakaoCallbackPage";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/member">
+          <Route path="login" element={<MemberLoginPage />} />
+          <Route path="join" element={<MemberJoinTermsPage />} />
+          <Route path="join/info" element={<MemberJoinPage />} />
+          <Route path="join/complete" element={<MemberJoinCompletePage />} />
+          <Route path="kakao/join" element={<MemberKakaoJoinPage />} />
+          <Route path="kakao/callback" element={<MemberKakaoCallbackPage />} />
+        </Route>
         <Route path="/*" element={<DefaultLayout />}>
-          <Route path="member">
-            <Route path="join" element={<MemberJoinPage />} />
-            <Route path="login" element={<MemberLoginPage />} />
-          </Route>
           <Route path="healthcare">
             <Route path="schedule" element={<ScheduleMainPage />} />
             {/* user */}
