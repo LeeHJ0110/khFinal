7e3262e2f3f232b82d98e73c4e6948a5d614db59
DESKTOP-R944F2E\user1 <hello>
2026-05-26 09:23:59 +0900

    feat : boardApu.js 작성

---
+++ frontEnd/front/src/features/board/api/boardApi.js
@@ -0,0 +1,20 @@
+import api from "../../../app/api/axios";
+
+export async function writeBoardApi(formData) {
+  return (
+    await api.post("/board/new", formData),
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
+}
+
+export async function updateBoardApi(boardId, formData) {
+  return await api.put(`/board/edit/${boardId}`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+}
