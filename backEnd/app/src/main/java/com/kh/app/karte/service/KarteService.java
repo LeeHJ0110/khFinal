18b324485594989c2479cf84d952b5a9cd63c42a
DESKTOP-HTUSJ3D\user1 <adskln;owiewoi@aiuhkds.com>
2026-06-10 11:46:17 +0900

    feat: 메시지 임시 연결

--- backEnd/app/src/main/java/com/kh/app/karte/service/KarteService.java
+++ backEnd/app/src/main/java/com/kh/app/karte/service/KarteService.java
@@ -11,6 +11,8 @@ import com.kh.app.karte.repository.KarteRepository;
 import com.kh.app.karte.repository.ScoreRepository;
 import com.kh.app.member.entity.MemberEntity;
 import com.kh.app.member.repository.MemberRepository;
+import com.kh.app.message.entity.MessageReasonType;
+import com.kh.app.message.service.SystemMessageService;
 import com.kh.app.pet.entity.PetEntity;
 import com.kh.app.pet.repository.PetRepository;
 import com.kh.app.petcare.dto.response.DiagnosisDetailResDto;
@@ -37,12 +39,17 @@ public class KarteService {
     private final ScoreRepository scoreRepository;
     private final DiagnosisReqRepository diagnosisReqRepository;
 
+    private final SystemMessageService systemMessageService;
+
     @Transactional
     public void write(KarteReqDto reqDto, String username) {
         MemberEntity memberEntity = memberRepository.findByUsername(username)
                 .orElseThrow(() -> new IllegalArgumentException("맴버 없음"));//TODO 권한 관리
         DiagnosisReqEntity diagEntity = diagnosisReqRepository.findById(reqDto.getDiaReqId())
                 .orElseThrow(() -> new IllegalArgumentException("신청내역 없음"));
+
+        String petName = diagEntity.getPetEntity().getName();
+        MemberEntity reqMember = diagEntity.getPetEntity().getMember();
 
         KarteEntity karte = karteRepository.save(reqDto.toEntity(memberEntity, diagEntity));
 
@@ -53,6 +60,14 @@ public class KarteService {
 
         scoreRepository.saveAll(scores);
         log.info("[진단결과 작성 완료] writer: {}", memberEntity);
+
+        systemMessageService.sendByAdmin(
+                username,
+                reqMember,
+                MessageReasonType.NOTICE,
+                "펫 진단 완료 알림",
+                petName + "의 온라인 진단이 완료되었습니다. 진단확인페이지에서 확인해주세요."
+        );
     }
 
     public Page<KarteListResDto> getKarteList(int pno, String username) {
