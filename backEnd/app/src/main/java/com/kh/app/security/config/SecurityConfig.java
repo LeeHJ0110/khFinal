45d5b0b79f1f621a7a45c0777fcfa32668a0a9e0
DESKTOP-HTUSJ3D\user1 <adskln;owiewoi@aiuhkds.com>
2026-06-11 12:53:54 +0900

    feat: axios ip 변경

--- backEnd/app/src/main/java/com/kh/app/security/config/SecurityConfig.java
+++ backEnd/app/src/main/java/com/kh/app/security/config/SecurityConfig.java
@@ -87,7 +87,8 @@ public class SecurityConfig {
 
                     config.setAllowedOrigins(List.of(
                             "http://localhost:5173",
-                            "http://127.0.0.1:5173"
+                            "http://127.0.0.1:5173",
+                            "https://petandifor.store"
                     ));
 
                     config.setAllowedMethods(List.of(
--- frontEnd/front/src/app/api/axios.js
+++ frontEnd/front/src/app/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://127.0.0.1/api`, //TODO IP 바꿔서 적용할것
+  baseURL: `https://petandifor.store/api`, //TODO IP 바꿔서 적용할것
 });
 
 api.interceptors.request.use((config) => {
