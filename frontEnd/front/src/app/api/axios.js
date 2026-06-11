621bee66f6953fce1a5bc1454a44e44865a45baf
DESKTOP-R944F2E\user1 <hello>
2026-06-11 14:23:02 +0900

    feat : .env 파일 추가 로컬, 실서버 둘다 잘되는지 테스트

--- frontEnd/front/src/app/api/axios.js
+++ frontEnd/front/src/app/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `http://127.0.0.1/api`, //TODO IP 바꿔서 적용할것
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
